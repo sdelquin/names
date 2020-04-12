9f3f55f75e1ac3ab425fedf9ba467ecda3aab1b5
Sergio Delgado Quintero <sdelquin@gmail.com>
2020-04-12 10:29:36 +0100

    Migrateo from pipenv to requirements

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,12 @@
-from fabric.api import env, local, cd, run
+from fabric.api import env, local, cd, run, prefix
 
 env.hosts = ['cloud']
 
 
 def deploy():
     local('git push')
-    with cd('~/names'):
-        run('git pull')
-        run('pipenv install')
-        run('supervisorctl restart names')
+    with prefix('source ~/.virtualenvs/names/bin/activate'):
+        with cd('~/code/names'):
+            run('git pull')
+            run('pip install -r requirements')
+            run('supervisorctl restart names')
