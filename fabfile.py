844bab97418aa004572b88c5860058e1e8942a39
Sergio Delgado Quintero <sdelquin@gmail.com>
2024-08-23 09:38:30 +0100

    Adapt code to local venv

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,11 @@
-from fabric.api import env, local, cd, run, prefix
+from fabric.api import cd, env, local, prefix, run
 
 env.hosts = ['sdelquin.me']
 
 
 def deploy():
     local('git push')
-    with prefix('source ~/.pyenv/versions/names/bin/activate'):
+    with prefix('source .venv/bin/activate'):
         with cd('~/code/names'):
             run('git pull')
             run('pip install -r requirements.txt')
